374e59fd293aa20646327a18ff008c7fdb41c39e
Victor Gabriel <03012008victorgabriel@gmail.com>
2026-01-09 11:06:40 +0000

    fix(i18n): show page title as 'Configurações' on /admin/settings

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -7,6 +7,11 @@ use Filament\Pages\Page;
 class Settings extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
+    /**
+     * Ensure the page header (title) is shown in Portuguese as well.
+     */
+    protected static ?string $title = 'Configurações';
+
     protected static ?string $navigationLabel = 'Configurações';
     protected static string $view = 'filament.pages.settings';
     protected static bool $shouldRegisterNavigation = false;
